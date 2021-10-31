169f9c0c9110baeb67aadbcf0763ae3421f8609b
Marcelo Santos <rocha.mer21@gmail.com>
2021-10-31 19:51:38 -0300

    createContato criado

--- Lista-de-Contatos/src/main/java/com/marcelo/listadecontatos/Lista/de/Contatos/controller/ContatosController.java
+++ Lista-de-Contatos/src/main/java/com/marcelo/listadecontatos/Lista/de/Contatos/controller/ContatosController.java
@@ -3,10 +3,7 @@ package com.marcelo.listadecontatos.Lista.de.Contatos.controller;
 import com.marcelo.listadecontatos.Lista.de.Contatos.model.Contatos;
 import com.marcelo.listadecontatos.Lista.de.Contatos.repository.ContatosRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,4 +20,11 @@ public class ContatosController {
     public List<Contatos> getAllContatos() {
         return contatosRepository.findAll();
     }
+
+    //add contatos
+    @PostMapping("/contatos")
+    public Contatos createContato(@RequestBody Contatos contato) {
+        return contatosRepository.save(contato);
+    }
+
 }
